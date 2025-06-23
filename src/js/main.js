b9f66cc58bb52598328627815ab5a77e37076e7e
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-23 10:03:04 +0300

    send HTTP

--- src/js/main.js
+++ src/js/main.js
@@ -239,4 +239,57 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+    //send HTTP request
+    function sendRequest() {
+        const messages = {
+            loading: 'Loading',
+            success: 'Thank you, we will contact you later',
+            failure: 'Something went wrong',
+        };
+
+        document.querySelectorAll('form').forEach((form) => {
+            postData(form);
+        });
+
+        function postData(form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('div');
+                statusMessage.classList.add('status');
+                statusMessage.textContent = messages.loading;
+                form.append(statusMessage);
+
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+
+                request.setRequestHeader('Content-type', 'application/json');
+                const formData = new FormData(form);
+
+                const object = {};
+
+                formData.forEach((value, key) => {
+                    object[key] = value;
+                });
+
+                const json = JSON.stringify(object);
+
+                request.send(json);
+
+                request.addEventListener('load', () => {
+                    if (request.status === 200) {
+                        statusMessage.textContent = messages.success;
+                        form.reset();
+                        setTimeout(() => {
+                            statusMessage.remove();
+                        }, 5000);
+                    } else {
+                        statusMessage.textContent = messages.failure;
+                    }
+                });
+            });
+        }
+    }
+    sendRequest();
 });
