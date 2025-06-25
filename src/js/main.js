3b578581284d1394b7acfeceb86a7230452863fa
Vasiliy Vinogradov <vase3k@gmail.com>
2025-06-25 19:52:54 +0300

    json-server

--- src/js/main.js
+++ src/js/main.js
@@ -271,4 +271,8 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 5000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then((data) => data.json())
+        .then((res) => console.log(res));
 });
